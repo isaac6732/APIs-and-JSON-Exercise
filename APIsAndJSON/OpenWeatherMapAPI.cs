55541b31977d831725719d2d1d32534fb3894738
Isaac Oquendo <ioalvarez378@gmail.com>
2024-03-01 15:39:31 -0500

    API

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,23 @@ namespace APIsAndJSON
 {
     internal class OpenWeatherMapAPI
     {
+        public static void GetWeather()
+        {
+            var  apiKey = "3e1aca0b70ea6d4beae468c8bd63808e";
+           
+            var client = new HttpClient();
+            
+            var cityName = "New York";
+            
+            var weatherEndpoint = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=imperial";
+           
+            string weatherResponse = client.GetStringAsync(weatherEndpoint).Result;
+            
+            var weatherObject = JObject.Parse(weatherResponse);
+            Console.WriteLine(weatherObject);
+            
+            //Note: Usa el string $ {} con sus valores para mirar solos los valores que quieres ver del API en el CW.
+            //Note:Usa el userInput para que diga la ciudad y el API dara las temperatura de la ciudad que el elija 17,18 pon el codigo.
+        }
     }
 }
--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
 
 namespace APIsAndJSON
 {
-    internal class RonVSKanyeAPI
+    internal  class RonVSKanyeAPI
     {
+        public static void Ron()
+        {
+            HttpClient client = new HttpClient();
+
+            string ronEndpoint = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            string ronResponse = client.GetStringAsync(ronEndpoint).Result;
+
+            JArray ronObject = JArray.Parse(ronResponse);
+            
+            Console.WriteLine($"Ron: {ronObject[0]}");
+        }
+        public static void Kanye()
+        {
+            var client = new HttpClient();
+
+            var kanyeEndpoint = "https://api.kanye.rest/";
+
+            var kanyeResponse = client.GetStringAsync(kanyeEndpoint).Result;
+
+            var kanyeObject = JObject.Parse(kanyeResponse);
+
+            Console.WriteLine($"Kanye: {kanyeObject["quote"]}");
+        }
+
+        public static void RonAndKanye()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Ron();
+                Kanye();
+                Console.WriteLine();
+            }
+        }
     }
 }
